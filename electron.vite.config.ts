0b3a05b16ebf8077cb68abee13b18e7ca28d2c67
Javlonbek Saydullaev <javlonbeksaydullaev@MacBook-Air-Javlonbek.local>
2026-02-09 02:28:32 +0300

    Add Electron and Vite configuration

---
+++ electron.vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'electron/main/index.ts')
+        }
+      }
+    }
+  },
+  preload: {
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'electron/preload/index.ts')
+        }
+      }
+    }
+  },
+  renderer: {
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/index.html')
+        }
+      }
+    }
+  }
+})
